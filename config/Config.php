b36037deab553c3705754b7f7dfbd8bd422d258a
Putu Wisnu Ardia Chandra <71444414+Putu-Wisnu-Ardia-Chandra@users.noreply.github.com>
2020-12-09 15:33:38 +0700

    commit

--- config/Config.php
+++ config/Config.php
@@ -1,23 +1,25 @@
 <?php
 
-class Config{
-    function  koneksi(){
-        $conn=new mysqli ("localhost","root",'',"puskesmas");
-        if($conn->connect_error){
-            $conn =die("Koneksi gagal : ". $conn->connect_error);
-
-        }
-        return $conn;
+class Config
+{
+  function  koneksi()
+  {
+    $conn = new mysqli("localhost", "root", '', "puskesmas");
+    if ($conn->connect_error) {
+      $conn = die("Koneksi gagal : " . $conn->connect_error);
     }
-    function auth(){
-        if(isset($_SESSION['login']['email'])){
-            return true;
-        }else{
-            return false;
-        }
+    return $conn;
+  }
+  function auth()
+  {
+    if (isset($_SESSION['login']['email'])) {
+      return true;
+    } else {
+      return false;
     }
-    function site_url(){
-        return "http://localhost/sipus/";
-    }
+  }
+  function site_url()
+  {
+    return "";
+  }
 }
-?>
